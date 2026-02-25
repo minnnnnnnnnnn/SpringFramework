02bba417f90c078e9629baf7b5e708802aaa5595
lenovo <seongmin2053@naver.com>
2026-02-25 17:36:36 +0900

    댓글 기능 설정 추가

---
+++ src/main/java/org/zerock/dto/ReplyDto.java
@@ -0,0 +1,25 @@
+package org.zerock.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ReplyDto {
+
+    private Long rno;
+    private String replyText;
+    private String replyer;
+    private LocalDateTime replyDate;
+    private LocalDateTime updateDate;
+    private boolean delFlag;
+
+    private Long bno;
+
+}
